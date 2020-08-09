619f824e239eea271cb6f75edf4d655194f1d48f
SergeChurkin-ca <sergechurkin@yahoo.com>
2020-08-09 01:57:37 -0400

    Updates

---
+++ precache-manifest.d44ebb1dd4c493a54ef488de9a83eb1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75e9c514f0ea21a71cd47548588223b2",
+    "url": "/sergechurkin-project-five/index.html"
+  },
+  {
+    "revision": "03c0a9d9856c044f836c",
+    "url": "/sergechurkin-project-five/static/css/main.78b9946a.chunk.css"
+  },
+  {
+    "revision": "25e30cdfd5d1183a4ef6",
+    "url": "/sergechurkin-project-five/static/js/2.2bc149f8.chunk.js"
+  },
+  {
+    "revision": "70d88436874ad6f0b4ff8dc0e5c19c4e",
+    "url": "/sergechurkin-project-five/static/js/2.2bc149f8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03c0a9d9856c044f836c",
+    "url": "/sergechurkin-project-five/static/js/main.de1f4c50.chunk.js"
+  },
+  {
+    "revision": "ace5d3a9cd7a94d3281b",
+    "url": "/sergechurkin-project-five/static/js/runtime-main.2885d1f4.js"
+  }
+]);
