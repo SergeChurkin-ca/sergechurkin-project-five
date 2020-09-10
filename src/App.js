4279f5fe16c42e077e783666dc223fb172f502fb
SergeChurkin-ca <sergechurkin@yahoo.com>
2020-09-10 10:53:18 -0400

    clean up

--- src/App.js
+++ src/App.js
@@ -4,115 +4,102 @@ import NoteForm from "./NewTourForm.js";
 import "./App.css";
 
 const Header = (props) => {
-    return ( <
-        header >
-        <
-        h1 > { props.title } < /h1> <
-        span className = "stats" > Total tours: { props.totalTours } < /span> <
-        ul >
-        <
-        li > < /li> <
-        li > Tour Name < /li> <
-        li > Date < /li> <
-        li > Duration < /li> <
-        li > Seats Total < /li> <
-        /ul> <
-        /header>
-    );
+  return (
+    <header>
+      <h1> {props.title} </h1>
+      <span className="stats"> Total tours: {props.totalTours} </span>
+      <ul>
+        <li></li>
+        <li> Tour Name </li>
+        <li>Date</li>
+        <li> Duration </li>
+        <li> Seats Total </li>
+      </ul>
+    </header>
+  );
 };
 
 class App extends Component {
-    constructor() {
-        super();
-        this.database = firebase.database().ref();
-        this.state = {
-            tours: [],
+  constructor() {
+    super();
+    this.database = firebase.database().ref();
+    this.state = {
+      tours: [],
+    };
+  }
+
+  componentDidMount() {
+    const dbRef = firebase.database().ref();
+
+    dbRef.on("value", (snapshot) => {
+      // checking changes in db
+
+      const data = snapshot.val();
+
+      const newToursAarray = [];
+
+      for (let inventoryName in data) {
+        const toursObject = {
+          id: inventoryName,
+          tours: data[inventoryName],
+          name: data[inventoryName].name,
+          seats: data[inventoryName].seats,
+          date: data[inventoryName].date,
+          duration: data[inventoryName].duration,
         };
-    }
+        newToursAarray.push(toursObject);
+      }
 
-    componentDidMount() {
-        const dbRef = firebase.database().ref();
+      this.setState({
+        tours: newToursAarray,
+      });
+    });
+  }
 
-        dbRef.on("value", (snapshot) => {
-            // checking changes in db
+  // **********REMOVING ADN ADDING TOURS****************
+  handleRemoveTour = (id) => {
+    this.database.child(id).remove();
+  };
 
-            const data = snapshot.val();
+  addTour = (
+    toursObjectName,
+    toursObjectDate,
+    toursObjectDuration,
+    toursObjectSeats
+  ) => {
+    // console.log(toursObject);
+    this.database.push().set({
+      name: toursObjectName,
+      date: toursObjectDate,
+      duration: toursObjectDuration,
+      seats: toursObjectSeats,
+    });
+  };
 
-            const newToursAarray = [];
+  render() {
+    return (
+      <div className="tourlist">
+        <Header title="Tour Inventory" totalTours={this.state.tours.length} />
 
-            for (let inventoryName in data) {
-                const toursObject = {
-                    id: inventoryName,
-                    tours: data[inventoryName],
-                    name: data[inventoryName].name,
-                    seats: data[inventoryName].seats,
-                    date: data[inventoryName].date,
-                    duration: data[inventoryName].duration,
-                };
-                newToursAarray.push(toursObject);
-            }
-
-            this.setState({
-                tours: newToursAarray,
-            });
-        });
-    }
-
-    // **********REMOVING ADN ADDING TOURS****************
-    handleRemoveTour = (id) => {
-        this.database.child(id).remove();
-    };
-
-    addTour = (
-        toursObjectName,
-        toursObjectDate,
-        toursObjectDuration,
-        toursObjectSeats
-    ) => {
-        // console.log(toursObject);
-        this.database.push().set({
-            name: toursObjectName,
-            date: toursObjectDate,
-            duration: toursObjectDuration,
-            seats: toursObjectSeats,
-        });
-    };
-
-    render() {
-        return ( <
-            div className = "tourlist" >
-            <
-            Header title = "Tour Inventory"
-            totalTours = { this.state.tours.length }
-            />
-
-            {
-                this.state.tours.map((toursObject) => {
-                    return ( <
-                        ul className = "inventoryItem"
-                        key = { toursObject.id } >
-                        <
-                        li >
-                        <
-                        button onClick = {
-                            () => this.handleRemoveTour(toursObject.id) } >
-                        X <
-                        /button> <
-                        /li> <
-                        li > { toursObject.name } < /li> <
-                        li > { toursObject.date } < /li> <
-                        li > { toursObject.duration }
-                        hrs < /li> <
-                        li > { toursObject.seats } < /li> <
-                        /ul>
-                    );
-                })
-            } <
-            NoteForm addTour = { this.addTour }
-            /> <
-            /div>
-        );
-    }
+        {this.state.tours.map((toursObject) => {
+          return (
+            <ul className="inventoryItem" key={toursObject.id}>
+              <li>
+                <button onClick={() => this.handleRemoveTour(toursObject.id)}>
+                  X
+                </button>
+              </li>
+              <li>{toursObject.name}</li>
+              <li>{toursObject.date}</li>
+              <li>{toursObject.duration} hrs</li>
+              <li>{toursObject.seats}</li>
+            </ul>
+          );
+        })}
+        <NoteForm addTour={this.addTour} />
+      </div>
+    );
+  }
 }
 
 export default App;
